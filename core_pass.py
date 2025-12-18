abeeae481d757a8b185e475e5f6a96c189d80ff9
Dwarakesh-V <vdwarakesh0211@gmail.com>
2025-12-18 23:16:19 +0530

    Added rolling memory

--- core_pass.py
+++ core_pass.py
@@ -3,6 +3,7 @@ from math import exp
 from typing import List
 from numpy import mean
 from torch import tensor
+from collections import deque
 
 # Downloaded
 from sentence_transformers import SentenceTransformer, util
@@ -13,6 +14,8 @@ from split_parse import split_parse
 
 MODEL = SentenceTransformer("all-MiniLM-L12-v2")
 HIGH_CONFIDENCE_THRESHOLD = 0.35
+PASS_PREV_DATA = []
+PREV_DATA = deque([None,None,None])
 
 def ct_query(query: str, base=0.5, decay=0.03, min_threshold=0.25)->float: # Confidence is a function based on length
     length = len(query.split())
@@ -46,46 +49,56 @@ def single_level_pass(node:node,enc_query:tensor,query:str=""):
 
     return val
 
-GLOBAL_PREV_DATA = []
-
 def depth_pass(enc_query:tensor,query:str="",level=0):
-    node = GLOBAL_PREV_DATA[-1]
-    print(f"Depth pass metadata\nRecursion level: {level}\nPrevious passes: {[node.intent for node in GLOBAL_PREV_DATA]}\n--------------------\n")
+    node = PASS_PREV_DATA[-1]
+    print(f"Depth pass metadata\nRecursion level: {level}\nPrevious passes: {[node.intent for node in PASS_PREV_DATA]}\n--------------------\n")
     hct = ct_query(query) # High confidence threshold
     lct = max(0,hct-0.2) # Low confidence threshold
     print(f"High confidence threshold: {hct} | Low confidence threshold: {lct}")
     confidences = single_level_pass(node,enc_query,query)
 
     if not any(confidences) > lct:
-        if len(GLOBAL_PREV_DATA)==1:
-            print("Your query was unable to be processed. Please try rephrasing it.")
-            return
-        else:
-            GLOBAL_PREV_DATA.pop(-1)
-            depth_pass(enc_query,query)
-            return
+        # Retrieval augmented fallback context - Top 3 historical intent matching
+        confs = []
+        for child in node.children:
+            confs.append(util.cos_sim(MODEL.encode(PREV_DATA),child.intent).max().item())
+        confidences[confs.index(max(confs))] += 0.1*max(confs)
+        if not any(confidences) > lct:
+            if len(PASS_PREV_DATA)==1:
+                print("Your query was unable to be processed. Please try rephrasing it.")
+                return
+            else:
+                PASS_PREV_DATA.pop(-1)
+                depth_pass(enc_query,query)
+                return
     
     if all(confidences) < hct and all(confidences) >= lct:
-        # Ask for clarification
-        print(f"You are currently in \"{node.intent}\" for query {query}\nWhat are you looking for?")
         for child in node.children:
-            print(f"{child.intent}")
-        user_clarify = input("You: ")
-        user_encode = MODEL.encode(user_clarify,convert_to_tensor=True)
-        depth_pass(node, user_encode, user_clarify,level+1)
-        return
+            confs.append(util.cos_sim(MODEL.encode(PREV_DATA),child.intent).max().item())
+        confidences[confs.index(max(confs))] += 0.1*max(confs)
+        if all(confidences) < hct and all(confidences) >= lct:
+            # Ask for clarification
+            print(f"You are currently in \"{node.intent}\" for query {query}\nWhat are you looking for?")
+            for child in node.children:
+                print(f"{child.intent}")
+            user_clarify = input("You: ")
+            user_encode = MODEL.encode(user_clarify,convert_to_tensor=True)
+            depth_pass(node, user_encode, user_clarify,level+1)
+            return
 
     for i in range(len(confidences)): # If multiple children have high confidences, pass it through all of them. If there are no children, then intent is completed.
         if confidences[i]>hct:
-            GLOBAL_PREV_DATA.append(node.children[i])
+            PASS_PREV_DATA.append(node.children[i])
+            PREV_DATA.append(node.children[i].intent) # Add string to PREV_DATA
+            PREV_DATA.popleft()
             # Pass with that node that root if it has children. If it has no children, return that result.
             depth_pass(enc_query,query,level+1)
     else:
         print(f"Completed \"{node.intent}\"")
 
 def iter_pass(user_query:str,node:node):
-    global GLOBAL_PREV_DATA
-    GLOBAL_PREV_DATA.append(node)
+    global PASS_PREV_DATA
+    PASS_PREV_DATA.append(node)
     queries = split_parse(user_query)
     for query,_ in queries:
         enc_query = MODEL.encode(query,convert_to_tensor=True)
